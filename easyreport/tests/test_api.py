cb8416e57379231f0c4bcb4d579567677e931d08
Alex Loew <a.loew@gmx.net>
2015-03-08 21:03:34 +0100

    further tests

--- easyreport/tests/test_api.py
+++ easyreport/tests/test_api.py
@@ -8,14 +8,55 @@ import sys
 cpath = os.path.dirname(os.path.realpath(__file__)) + os.sep + '..' + os.sep + '..'
 sys.path.insert(0,cpath)
 
-from easyreport import EasyReport
+from easyreport import EasyReport, Feature
+import tempfile
 
-class TestData(unittest.TestCase):
+class TestData(TestCase):
 
     def setUp(self):
-        pass
+        self.file = tempfile.mktemp()
+        self.yml = self.file + '.yml'
 
     def test_init(self):
+        # invalid init
+        try:
+            R = EasyReport()
+            error_thrown = False
+        except:
+            error_thrown = True
+        self.assertTrue(error_thrown)
+
+        # init
+        R = EasyReport(file=self.file)
+        self.assertEqual(R.file, self.yml)
+        self.assertTrue(hasattr(R, 'sections'))
+
+    def test_section(self):
+        R = EasyReport(file=self.file)
+        try:
+            R.add('models', 1)   # provide an invalid value
+            error_thrown = False
+        except:
+            error_thrown = True
+        self.assertTrue(error_thrown)
+
+        F = Feature(self.yml)
+        R.add('models', (1,2))  # add a tuple as test
+        R.add('models', F)  # add feature
+        R.add('observations', (3,4))
+        R.add('misc', (5,6))
+
+        self.assertEqual(len(R.sections.keys()),3)
+        for k in R.sections.keys():
+            self.assertTrue(k in ['models', 'observations', 'misc'])
+
+
+
+    def test_save(self):
+        R = EasyReport(file=self.file)
+        R.save()
+        self.assertTrue(os.path.exists(self.yml))
+
 
 
 
